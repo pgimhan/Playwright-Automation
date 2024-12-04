e2b5b3965c503b35c55e3fcf98143916f01c27bf
PrabuGimhan <pravindugimhan@gmail.com>
2024-12-04 17:42:56 +0530

    auto waiting and alternative waits

---
+++ tests/autoWaiting.spec.ts
@@ -0,0 +1,33 @@
+import {test,expect} from '@playwright/test'
+
+test.beforeEach(async ({page})=>{
+    await page.goto('http://uitestingplayground.com/ajax')
+    await page.getByText('Button Triggering AJAX Request').click()
+})
+
+test('auto waiting',async ({page})=>{
+    const successButton = page.locator('.bg-success')
+
+    // await successButton.click()
+
+    // await successButton.waitFor({state:'attached'})
+    // const text = await successButton.allTextContents()
+    // expect(text).toContain('Data loaded with AJAX get request.')
+
+    await expect(successButton).toHaveText('Data loaded with AJAX get request.',{timeout:20000})
+})
+
+
+test('alternative waits',async({page})=>{
+    const successButton = page.locator('.bg-success')
+
+    //__ wait for element
+    // await page.waitForSelector('.bg-success')
+
+    //__ wait for response
+    await page.waitForResponse('http://uitestingplayground.com/ajaxdata')
+
+    const text = await successButton.allTextContents()
+    expect(text).toContain('Data loaded with AJAX get request.')
+
+})
