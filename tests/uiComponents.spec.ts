c6d4362da35666440a2b68ed3b08b5cd38b196d3
prabugimhan <pravindugimhan@gmail.com>
2025-02-20 18:20:58 +0530

    drop down and list validation 1

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -54,3 +54,38 @@ test('checkboxes',async ({page})=>{
         expect(await box.isChecked()).toBeTruthy()
     }
 })
+
+test('list and dropdown', async ({page})=>{
+    const dropDownMenu = page.locator('ngx-header nb-select')
+    await dropDownMenu.click()
+
+    page.getByRole('list')  //when the list has a UL tag    
+    page.getByRole('listitem') //when the list has LI tag
+
+    // const optionList = page.getByRole('list').locator('nb-option')
+    const optionList = page.locator('nb-option-list nb-option')
+    await expect(optionList).toHaveText(['Light','Dark','Cosmic','Corporate'])
+
+    await optionList.filter({hasText:'Cosmic'}).click()
+    const header = page.locator('nb-layout-header')
+    await expect(header).toHaveCSS('background-color','rgb(50, 50, 89)')
+
+
+    const colors ={
+        'Light': 'rgb(255, 255, 255)',
+        'Dark' : 'rgb(25, 32, 56)',
+        'Cosmic': 'rgb(50, 50, 89)',
+        'Coperate': 'rgb(255, 255, 255)'
+    }
+
+    await dropDownMenu.click()
+    for(const color in colors){
+        await optionList.filter({hasText:color}).click()
+        await expect(header).toHaveCSS('background-color', colors[color])
+        if (color != 'Coperate')
+            await dropDownMenu.click()
+
+    }
+
+    // for in use beacuse of object array
+})
