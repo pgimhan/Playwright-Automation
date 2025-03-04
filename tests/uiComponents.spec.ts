91773b134237807c830b5c2196ab8a42977c4f57
prabugimhan <pravindugimhan@gmail.com>
2025-03-04 12:34:05 +0530

    dialog box validation

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -103,3 +103,16 @@ test('tooltip',async({page})=>{
     expect(toolTip).toEqual('This is a tooltip')
 
 })
+
+test('dialog box',async({page})=>{
+    await page.getByText('Tables & Data').click()
+    await page.getByText('Smart Table').click()
+
+    page.on('dialog', dialog =>{
+        expect(dialog.message()).toEqual('Are you sure you want to delete?')
+        dialog.accept()
+    })
+
+    await page.getByRole('table').locator('tr',{hasText:'mdo@gmail.com'}).locator('.nb-trash').click()
+    await expect(page.locator('table tr').first()).not.toHaveText('mdo@gmail.com')
+})
