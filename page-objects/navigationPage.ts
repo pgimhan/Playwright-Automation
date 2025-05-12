410c3db7598b2d5caaa4811c77a2fc3ed8647ed2
pgimhan <pravindugimhan@gmail.com>
2025-05-12 10:19:33 +0530

    Added Helper methode for navigationPage

--- page-objects/navigationPage.ts
+++ page-objects/navigationPage.ts
@@ -9,7 +9,37 @@ export class NavigationPage{
     }
 
     async formLayoutPage(){
-        await this.page.getByText('forms').click()
-        await this.page.getByText('form layouts').click()
+        await this.selectGroupMenuItem('Forms')
+        await this.page.getByText('Form layouts').click()
+    }
+
+    async datePickerPage(){
+        await this.selectGroupMenuItem('Forms')
+        await this.page.getByText('Datepicker').click()
+    }
+
+    async smartTablePage(){
+        await this.selectGroupMenuItem('Tables & Data')
+        await this.page.getByText('Smart Table').click()
+    }
+
+    async toastrPage(){
+        await this.selectGroupMenuItem('Modal')
+        await this.page.getByText('Toastr').click()
+    }
+
+    async toolTipPage(){
+        await this.selectGroupMenuItem('Modal & Overlays')
+        await this.page.getByText('Tooltip').click()
+    }
+
+    private async selectGroupMenuItem(groupItemTiltle: string){
+        const groupMenuItem = this.page.getByTitle(groupItemTiltle)
+        const expandedState = await groupMenuItem.getAttribute('aria-expanded')
+
+        if(expandedState == 'false'){
+            await groupMenuItem.click()
+        }
     }
 }
+
