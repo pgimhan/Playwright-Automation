4ca223611df26a20344a34a94a93a5a73cece63f
pgimhan <pravindugimhan@gmail.com>
2025-06-26 21:03:59 +0530

    Date Picker Dynamic Handeling

--- tests/uiComponents.spec.ts
+++ tests/uiComponents.spec.ts
@@ -165,7 +165,23 @@ test('date picker',async({page})=>{
 
    const calanderInput = page.getByPlaceholder('Form Picker')
    await calanderInput.click()
-   await page.locator('[class="day-cell ng-star-inserted"]').getByText('14',{exact:true}).click()
-   await expect(calanderInput).toHaveValue('Jun 14, 2025')
 
+   let date = new Date()
+   date.setDate(date.getDate() + 300)
+   const expectedDate = date.getDate().toString()
+   const expectedMonthShort = date.toLocaleString('En-US',{month:'short'})
+   const expectedMonthLong = date.toLocaleString('En-US',{month:'long'})
+   const expectedYear = date.getFullYear()
+   const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`
+
+   let calanderMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+   const expectedMonthAndYear = ` ${expectedMonthLong} ${expectedYear}`
+   while(!calanderMonthAndYear.includes(expectedMonthAndYear)){
+        await page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click()
+        calanderMonthAndYear = await page.locator('nb-calendar-view-mode').textContent()
+   }
+
+   await page.locator('[class="day-cell ng-star-inserted"]').getByText(expectedDate,{exact:true}).click()
+   await expect(calanderInput).toHaveValue(dateToAssert) 
+   
 })
